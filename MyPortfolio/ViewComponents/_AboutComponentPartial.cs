dbd0d5c5e7358db9dfbd6f9599cba403c878d88c
ErsinAgirman <117252660+ErsinAgirman@users.noreply.github.com>
2024-04-03 19:35:05 +0300

    View Componet added.

---
+++ MyPortfolio/ViewComponents/_AboutComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _AboutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyPortfolio/ViewComponents/_PortfolioComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _PortfolioComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
